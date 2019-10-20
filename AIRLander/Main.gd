808e5f2385beb9a4e4eb266fe1e2d8157ee98abb
Daniel <rodriguez.garcia.dan@gmail.com>
2019-10-20 02:49:35 +0200

    instantiating working for the first time

--- AIRLander/Main.gd
+++ AIRLander/Main.gd
@@ -3,21 +3,30 @@ extends Node
 # Declare member variables here. Examples:
 # var a = 2
 # var b = "text"
-var s = load("res://Starship.gd")
+onready var s = load("res://Starship.gd")
+
+onready var Starship_scene = preload("res://Starship.tscn")
+
 var pos = Vector2(10.0,10.0)
 var vel = Vector2(0.0,-300.0)
 var gamma = Vector2(0.0,0.0)
-var nuevas = s.new(pos,vel,gamma)
+var s1
+var ss
+#var s2 = s.new(Vector2(50.0,30.0),vel,gamma)
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	
-	
-	
-	nuevas.update()
+	s1 = s.new(pos,vel,gamma)
+	s1.name = "hola"
+	add_child(s1)
+	s1.crear()
+	s1.update()
+	#ss = Starship_scene.instance()
+	#add_child(ss)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	nuevas.update()
-	print(nuevas.r,nuevas.theta)
+	s1.update()
+	#print(nuevas.r,nuevas.theta)
--- AIRLander/Starship.gd
+++ AIRLander/Starship.gd
@@ -1,6 +1,8 @@
 extends Spatial
 
 class_name Starship
+
+
 var r :Vector2
 var v :Vector2
 var gamma :Vector2
@@ -21,63 +23,74 @@ var d_rcs
 var fuerzas
 var ri
 var Dt
+onready var ss = preload("res://Starship.tscn").instance()
+var ssrot
+
+
+
 
 
 func _init(r,v,gamma,alpha = 0.0, w = 0.0, theta = 0.0):
-	self.r = r
-	self.v = v
-	# self.rglobal
-	# self.vglobal
-	# self.gammaglobal
-	# self.vlocal
-	# self.gammalocal
-	self.gamma = gamma #aceleracion
-	self.alpha = alpha #acc angular
-	self.w = w #vel angular
-	self.theta = theta #ángulo
+	
+	#ss = ss_scene.instance()
+	
+	
+	r = r
+	v = v
+	# rglobal
+	# vglobal
+	# gammaglobal
+	# vlocal
+	# gammalocal
+	gamma = gamma #aceleracion
+	alpha = alpha #acc angular
+	w = w #vel angular
+	theta = theta #ángulo
 	
 	
 	
 	#Modelo Cilíndrico, CG en el centro Geométrico (0,0) en coord locales
-	self.diametro = 9.0 #diámetro
-	self.radio = self.diametro/2.0 #radio
-	self.h = 50 #Longitud del cohete
+	diametro = 9.0 #diámetro
+	radio = diametro/2.0 #radio
+	h = 50 #Longitud del cohete
 	
-	self.m = 120000
+	m = 120000
 	
 	#Tensor de inercia de un cilindro (Cambiar cuando cambie el modelo a uno más realista)
-	self.Itensor = Basis()
-	self.Itensor.x = (self.m/12.0)*Vector3(3*pow(self.radio,2) + self.h, 0, 0)
-	self.Itensor.y = (self.m/12.0)*Vector3(0,3*pow(self.radio,2) + self.h, 0)
-	self.Itensor.z = (self.m/12.0)*Vector3(0, 0, 6*pow(self.radio,2))
+	Itensor = Basis()
+	Itensor.x = (m/12.0)*Vector3(3*pow(radio,2) + h, 0, 0)
+	Itensor.y = (m/12.0)*Vector3(0,3*pow(radio,2) + h, 0)
+	Itensor.z = (m/12.0)*Vector3(0, 0, 6*pow(radio,2))
 	print(Itensor)
-	self.I2D = self.Itensor.x.x #El valor que vamos a usar en el problema 2D, es la única dirección en la que lo giraremos
-#	print(self.I2D)
-	self.beta = 0 #Ángulo de la tobera del motor respecto al eje longitudinal
-	self.Ftmax = 6000000  #Máximo empuje del motor cohete
+	I2D = Itensor.x.x #El valor que vamos a usar en el problema 2D, es la única dirección en la que lo giraremos
+#	print(I2D)
+	beta = 0 #Ángulo de la tobera del motor respecto al eje longitudinal
+	Ftmax = 1200000  #Máximo empuje del motor cohete
 	
 	#RCS -- en el extremo superior, apuntando a -x y +x
-	self.Frcs = [Vector2(-0.01,0),Vector2(0.01,0)] #Vectores de empuje del control de actitud (RCS)
-	self.RCS = [1,0] #indica qué RCS están activos (1) e inactivos (0)
-	self.d_rcs = self.h/2 #distancia de los rcs respecto al CG (h/2---- extremo superior del cohete)
+	Frcs = [Vector2(-0.01,0),Vector2(0.01,0)] #Vectores de empuje del control de actitud (RCS)
+	RCS = [1,0] #indica qué RCS están activos (1) e inactivos (0)
+	d_rcs = h/2 #distancia de los rcs respecto al CG (h/2---- extremo superior del cohete)
 	
 	
-	self.fuerzas = [] #Aquí entrarán las fuerzas en cada momento para iterar en ellas
-	self.ri = [] #Brazos de las fuerzas respecto al CM (para calcular los momentos)
-	self.Dt = 0.1 #Paso de tiempo en segundos
+	fuerzas = [] #Aquí entrarán las fuerzas en cada momento para iterar en ellas
+	ri = [] #Brazos de las fuerzas respecto al CM (para calcular los momentos)
+	Dt = 0.1 #Paso de tiempo en segundos
 	
 	var Sfuerzas = Vector2(0.0,0.0)
+	
+	
 
 func update():
 	#TODO: aplicar fuerzas, cambiar acc, 
 	
 	#clear f
-	self.fuerzas = []
-	self.ri = []
-	self.applyThrust(self.beta)
-	self.applyRCS()
-	self.applyG()
-	#print('ri:', self.ri)
+	fuerzas = []
+	ri = []
+	applyThrust(beta)
+	applyRCS()
+	applyG()
+	#print('ri:', ri)
 	
 	
 	#Newton
@@ -85,48 +98,57 @@ func update():
 	for f in fuerzas:
 		Sfuerzas += f
 	
-	print(Sfuerzas)
-	self.gamma = (1/self.m)*Sfuerzas
-	#print("gamma",self.gamma)
+	#print(Sfuerzas)
+	gamma = (1/m)*Sfuerzas
+	#print("gamma",gamma)
 	
 	#Momentos
-	self.alpha = 0
-	for i in range(len(self.fuerzas)):
+	alpha = 0
+	for i in range(len(fuerzas)):
 #		print(ri[i],fuerzas[i])
-		self.alpha += ri[i].cross(fuerzas[i]) #
+		alpha += ri[i].cross(fuerzas[i]) #
 #		print("Cross")
 #		print(ri[i].cross(fuerzas[i]))
 	#velocidad
-	self.v += self.gamma*self.Dt
+	v += gamma*Dt
 	#posicion
-	self.r += self.v *self.Dt
+	r += v *Dt
 	
 	#vel. angular
-	self.w += self.alpha *self.Dt
+	w += alpha *Dt
 	#angulo theta
-	self.theta += self.w*self.Dt
-	
-	
-	self.alpha *= (1/self.I2D)
+	theta += w*Dt
+	alpha *= (1/I2D)
+	applyUpdate()
+
 	
 	
 	
 func applyThrust(beta):
-    var f = Vector2(-self.Ftmax*sin(beta), self.Ftmax*cos(beta))
-    self.fuerzas.append(f)
-    var brazo = Vector2(sin(beta)*cos(beta)*self.h/2.0, sin(beta)*sin(beta)*self.h/2.0)
-    self.ri.append(brazo)
+    var f = Vector2(-Ftmax*sin(beta), Ftmax*cos(beta))
+    fuerzas.append(f)
+    var brazo = Vector2(sin(beta)*cos(beta)*h/2.0, sin(beta)*sin(beta)*h/2.0)
+    ri.append(brazo)
 
 func applyRCS():
 	for i in range(len(RCS)):
 		if RCS[i]:
-			self.fuerzas.append(self.Frcs[i])
-			self.ri.append(Vector2(0,self.d_rcs))
+			fuerzas.append(Frcs[i])
+			ri.append(Vector2(0,d_rcs))
     
 func applyG():
 	
 	fuerzas.append(m*Vector2(0,-10))
 	ri.append(Vector2(0.0,0.0))
 
-func magnitud2D(vector:Vector2):
-	return sqrt(vector[0]*vector[0] + vector.y*vector.y)
+
+func applyUpdate():
+	#print('rot',ssrotational)
+	ss.set_translation(Vector3(r.x,r.y,0))
+	ssrot.rotate(Vector3(0,0,1),deg2rad(w))
+	
+
+func crear():
+	add_child(ss)
+	ssrot = get_node("Starship/Starship-rotation")
+	ss.set_translation(Vector3(r.x,r.y,0))
